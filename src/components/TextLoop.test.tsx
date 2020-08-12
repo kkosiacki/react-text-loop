76a6714274c4f48fa058914af3de43c7883eb03b
Karol Kosiacki <karol.kosiacki@inpay.pl>
2020-08-12 16:11:27 +0200

    fix tests and add install command

--- src/components/TextLoop.test.tsx
+++ src/components/TextLoop.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
-import TextLoop from "./TextLoop";
+import {TextLoop} from "./TextLoop";
 
 afterEach(() => {
     cleanup();
